a0f18a5c6a533bc8bb407032e06272d49db741e6
Daniel Golub <danielg6666@gmail.com>
2023-05-19 16:07:31 +0300

    feat: added exposed classes to the package

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,21 +1,25 @@
 import { strict as assert } from 'node:assert';
 import pkg from '../../index.js';
-import EndpointError from '../../lib/errors/EndpointErrors.js';
-import ParameterError from '../../lib/errors/ParameterErrors.js';
+import ParameterError from '../../lib/errors/ParameterError.js';
 import Endpoint from '../../lib/Endpoint.js';
 import ParameterValidator from '../../lib/ParameterValidator.js';
 import ExpressMiddleware from '../../lib/integrators/ExpressMiddleware.js';
+import SecurityError from '../../lib/errors/SecurityError.js';
+import MiddlewareError from '../../lib/errors/MiddlewareError.js';
+import SecurityValidator from '../../lib/SecurityValidator.js';
 
 describe('unit: index', () => {
   it('should expose the correct package output', () => {
     assert.deepEqual(pkg, {
       errors: {
-        EndpointError,
         ParameterError,
+        SecurityError,
+        MiddlewareError,
       },
       modules: {
         Endpoint,
         ParameterValidator,
+        SecurityValidator,
       },
       ExpressMiddleware,
     });
