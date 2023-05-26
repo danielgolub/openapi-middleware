19ffa09ae4d5313df34e8ecfff15bea916aeceed
Daniel Golub <danielg6666@gmail.com>
2023-05-27 00:05:36 +0300

    docs: added doc for index file

--- index.js
+++ index.js
@@ -5,17 +5,43 @@ import ParameterError from './lib/errors/ParameterError.js';
 import SecurityError from './lib/errors/SecurityError.js';
 import MiddlewareError from './lib/errors/MiddlewareError.js';
 import SecurityValidator from './lib/SecurityValidator.js';
+import ResponseValidator from "./lib/ResponseValidator.js";
 
+/**
+ * @module
+ */
 export default {
+  /**
+   * @type {object}
+   * @description errors custom error objects
+   * @property {module:ParameterError} ParameterError input validator error
+   * @property {module:SecurityError} SecurityError security validator error
+   * @property {module:MiddlewareError} MiddlewareError express error
+   */
   errors: {
     ParameterError,
     SecurityError,
     MiddlewareError,
   },
+
+  /**
+   * @type {object}
+   * @description standalone modules for openapi validation
+   * @property {module:Endpoint} Endpoint endpoint validator class (integrates all the other validators)
+   * @property {module:ParameterValidator} ParameterValidator inputs validator per endpoint
+   * @property {module:SecurityValidator} SecurityValidator security validator per endpoint
+   * @property {module:ResponseValidator} ResponseValidator output validator per endpoint
+   */
   modules: {
     Endpoint,
     ParameterValidator,
     SecurityValidator,
+    ResponseValidator,
   },
+
+  /**
+   * @type {module:ExpressMiddleware}
+   * @description express router for validating your openapi definitions
+   */
   ExpressMiddleware,
 };
